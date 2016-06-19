6ac9752f15ac85bf8a33d75966386ed7c8298ada
谢靖伦 <jinglun.xie@corp.elong.com>
2016-06-19 20:34:52 +0800

    install oh-my-zsh

--- development_env_ready.sh
+++ development_env_ready.sh
@@ -17,49 +17,73 @@ distribution_name=$(get_distribution_name)
 
 # install redis
 install_redis(){
+  redis_dir_name=/usr/local/redis-3.2.0
   echo "Install redis by compiling from source code...\n
   version: 3.2.0\n"
-  wget "http://download.redis.io/releases/redis-3.2.0.tar.gz"
-  tar -xzvf redis-3.2.0.tar.gz -C /usr/local/
-  cd /usr/local/redis-3.2.0  
+  if [ -d "$redis_dir_name" ];then
+    echo "Already Downloaded!"
+  else
+    wget "http://download.redis.io/releases/redis-3.2.0.tar.gz"
+    tar -xzvf redis-3.2.0.tar.gz -C /usr/local/
+  fi
+  cd "$redis_dir_name"
   make 
+  make install
+  cd 
 }
 
 # install ruby
 install_ruby(){
+  ruby_dir_name="ruby-2.3.1"
   echo "Install ruby by compiling from source code...\n
   version: 2.3.1\n"
-  wget "https://cache.ruby-lang.org/pub/ruby/2.3/ruby-2.3.1.tar.gz"
-  tar -xzvf ruby-2.3.1.tar.gz 
-  cd ruby-2.3.1
+  if [ -d "$ruby_dir_name" ];then
+    echo "Already downloaded!"
+  else
+    wget "https://cache.ruby-lang.org/pub/ruby/2.3/ruby-2.3.1.tar.gz"
+    tar -xzvf ruby-2.3.1.tar.gz
+  fi
+  cd $dir_name
   ./configure
   make 
   make install
+  cd 
 }
 
-# install stuff (git, nginx, mysql, redis, openssl ruby)
+# install oh-my-zsh 
+install_oh_my_zsh(){
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+}
+# install stuff (zsh, mosh, git, nginx, mysql, redis, openssl ruby)
 install_stuff(){
   case "$distribution_name" in
     'mac')
       brew update
-      brew install git nginx mysql redis
+      brew install mosh zsh git nginx mysql redis
       brew install openssl
+      brew install nodejs
       ;;
     'ubuntu')
       apt-get update
-      apt-get install build-essential
-      apt-get install git nginx mysql-client mysql-server libmysql++-dev libmysqlclient-dev libmysql++3
+      apt-get -y install build-essential
+      apt-get -y install imagemagick libmagick++-dev
+      apt-get -y install mosh zsh git nginx mysql-client mysql-server libmysql++-dev libmysqlclient-dev libmysql++3
       install_redis
-      apt-get install openssl libssl-dev
+      apt-get -y install openssl libssl-dev
+      # for js runtime 
+      apt-get -y install nodejs
       install_ruby
       ;;
     'centos')
       yum update
-      yum install git nginx mysql-client mysql-server
+      yum -y install mosh zsh git nginx mysql-client mysql-server
       install_redis
-      yum install openssl openssl-devel
+      yum -y install openssl openssl-devel
+      yum -y install nodejs
       ;;
   esac
 }
 
 install_stuff
+install_oh_my_zsh
