239ef5b3b9d9822df2cf6be5a457cffc48b768f5
谢靖伦 <tinglenxan@gmail.com>
2017-11-05 16:42:39 +0800

    install zlib

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 ### My WorkingSpace
 
-#### what included ?
+#### What included ?
 
 * One step shell script to ready my development environment.
 
 * `vimrc` and `zshrc`
 
-* Beautiful oh-my-zsh theme `powerline.zsh-theme`
+* Beautiful [oh-my-zsh](https://github.com/robbyrussell/oh-my-zsh) theme `powerline.zsh-theme`
 
 * Other useful files
--- development_env_ready.sh
+++ development_env_ready.sh
@@ -1,7 +1,7 @@
-plantform_name=$(python -mplatform | tr '[:upper:]' '[:lower:]') 
+platform_name=$(python -mplatform | tr '[:upper:]' '[:lower:]')
 
 get_distribution_name(){
-  case "$plantform_name" in
+  case "$platform_name" in
     *darwin*)
       echo "mac"
       ;;
@@ -18,7 +18,8 @@ distribution_name=$(get_distribution_name)
 # Install redis by compiling source code
 
 install_redis(){
-  redis_version="3.2.0"
+  echo "Which redis version you want to install?\n"
+  read -p "-->: " redis_version
   echo "Install redis by compiling from source code...\nversion: $redis_version\n"
   if [ -f "redis-$redis_version.tar.gz" ];then
     tar -xzvf redis-$redis_version.tar.gz -C /usr/local/
@@ -35,12 +36,13 @@ install_redis(){
 # Install ruby 2.3.1 by compiling source code 
 
 install_ruby(){
-  ruby_version="2.3.1"
+  echo "Which Ruby version you want to install?\n"
+  read -p "-->: " ruby_version
   echo "Install ruby by compiling from source code...\nversion: $ruby_version\n"
   if [ -f "ruby-$ruby_version.tar.gz" ];then
     tar -xzvf ruby-$ruby_version.tar.gz
   else
-    wget "https://cache.ruby-lang.org/pub/ruby/2.3/ruby-$ruby_version.tar.gz"
+    wget "https://cache.ruby-lang.org/pub/ruby/${ruby_version:0:3}/ruby-$ruby_version.tar.gz"
     tar -xzvf ruby-$ruby_version.tar.gz
   fi
   cd ruby-$ruby_version
@@ -53,7 +55,7 @@ install_ruby(){
 # Install some important gems when ruby is ready
 
 install_gems(){
-  gem install bundler rails
+  gem install bundler
 }
 
 # Install amazing tool `oh-my-zsh` 
@@ -74,7 +76,9 @@ install_stuff(){
     'ubuntu')
       apt-get update
       apt-get -y install build-essential
-      apt-get -y install mosh zsh git nginx mysql-client mysql-server libmysql++-dev libmysql++3
+      apt-get -y install mosh zsh git nginx mysql-client mysql-server
+      apt-get -y install zlib1g zlib1g-dev
+      apt-get -y install libmysql++-dev
       apt-get -y install openssl libssl-dev nodejs
       install_redis
       install_ruby
